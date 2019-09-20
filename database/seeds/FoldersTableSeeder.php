49cafef5c2d793c089a879ad5c45f70bfea15ea3
vagrant <vagrant@homestead>
2019-09-20 05:34:28 +0000

    chapter08

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -2,6 +2,7 @@
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+
 class FoldersTableSeeder extends Seeder
 {
     /**
@@ -11,11 +12,13 @@ class FoldersTableSeeder extends Seeder
      */
     public function run()
     {
+      $user = DB::table('users')->first();
       $titles = ['プライベート', '仕事', '旅行'];
 
       foreach ($titles as $title) {
           DB::table('folders')->insert([
               'title' => $title,
+              'user_id' => $user->id,
               'created_at' => Carbon::now(),
               'updated_at' => Carbon::now(),
           ]);
